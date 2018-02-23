1863944690cf76e0e3c9fead88f1fbc6b79bc9f2
Roman Zimmermann <roman@more-onion.com>
2018-02-23 13:56:43 +0100

    Read werbecode from $submission->tracking->other.

---
+++ tests/ApiTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\enterbrain;
+
+use Drupal\little_helpers\Webform\Submission;
+use Drupal\little_helpers\Webform\Webform;
+
+/**
+ * Test the enterbrain API client.
+ */
+class ApiTest extends \DrupalUnitTestCase {
+
+  /**
+   * Test generating sonstigeInfo data.
+   */
+  public function testSonstigeInfo() {
+    $submission = $this->getMockBuilder(Submission::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $submission->webform = $this->getMockBuilder(Webform::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $submission->tracking = (object) [
+      'other' => 'wc_test',
+    ];
+    $api = $this->getMockBuilder(Api::class)
+      ->disableOriginalConstructor()
+      ->setMethods(NULL)
+      ->getMock();
+    $info = $api->sonstigeInfo($submission);
+    $this->assertEqual("WCintern=wc_test, Verwendungszweck: , Newsletter: false, CRM-ID: ", $info);
+  }
+
+}
